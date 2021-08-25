1209b0570b00b5fbc2007a71079e490dc8c5b6c3
Firemarcel112 <firemarcel112@gmail.com>
2021-08-25 11:51:59 +0200

    -

--- system/v1/_system/apps.js
+++ system/v1/_system/apps.js
@@ -1,13 +1,3 @@
-const apps = {
-    "system": "/../system/v1/systemapps/",
-    "apps": "/../system/v1/app/",
-
-    "settings": "settings",
-    "browser": "browser",
-    "store": "store",
-    "number": "guessnumber"
-}
-
 function o_settings() {
     window.location = "/system/v1/systemapps/settings/"
 }
@@ -22,35 +12,4 @@ function o_browser() {
 
 function o_number() {
     window.location = "/../system/v1/app/guessnumber/"
-}
-
-function desktopApps() {
-
-    const systemapp_settings = document.querySelector("#systemapp_settings");
-    systemapp_settings.href = apps.system + apps.settings;
-
-    const systemapp_store = document.querySelector("#systemapp_store");
-    systemapp_store.href = apps.system + apps.store;
-
-    const app_browser = document.querySelector("#app_browser");
-    app_browser.href = apps.apps + apps.browser;
-
-    const app_number = document.querySelector("#app_number");
-    app_number.href = apps.apps + apps.number;
-}
-
-
-function menuApps() {
-
-    const menu_systemapp_settings = document.querySelector("#menu_settings");
-    menu_systemapp_settings.href = apps.system + apps.settings;
-
-    const app_browser = document.querySelector("#menu_browser");
-    app_browser.href = apps.apps + apps.browser;
-
-    const systemapp_store = document.querySelector("#menu_store");
-    systemapp_store.href = apps.system + apps.store;
-
-    const app_number = document.querySelector("#menu_number");
-    app_number.href = apps.apps + apps.number;
 }
