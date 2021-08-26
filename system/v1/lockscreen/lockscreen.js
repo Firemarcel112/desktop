ec358db47380321be356ee9e3d40264bb5007113
firemarcel112 <marcel.bartl2@outlook.de>
2021-08-26 19:38:53 +0200

    -

--- system/v1/lockscreen/lockscreen.js
+++ system/v1/lockscreen/lockscreen.js
@@ -1,5 +1,5 @@
 function checkLogedIn() {
-    if(localStorage.getItem("username") == null || localStorage.getItem("password") == null) {
+    if (localStorage.getItem("username") == null || localStorage.getItem("password") == null) {
 
         const login_form = document.querySelector("#login");
         login_form.style.display = "none";
@@ -10,8 +10,8 @@ function checkLogedIn() {
         const register_form = document.querySelector("#register");
         register_form.style.display = "unset";
 
-         // Hinweise New Register //
-        alert("Du besitz keinen Account, bitte erstelle dir einen!");
+        // Hinweise New Register //
+        alert("Du besitzt keinen Account, bitte erstelle dir einen!");
     } else {
         const siteTitle = document.querySelector("title");
         const siteIcon = document.querySelector("#icon");
@@ -35,16 +35,16 @@ function checkLogedIn() {
 
 function isExistAccount() {
 
-    if(document.querySelector("#register").style.display == "unset") {
+    if (document.querySelector("#register").style.display == "unset") {
         return;
     } else {
 
-        if(localStorage.getItem("username") == null) {
+        if (localStorage.getItem("username") == null) {
             alert("Username nicht vorhanden")
             window.location.reload();
         }
-    
-        if(localStorage.getItem("password") == null) {
+
+        if (localStorage.getItem("password") == null) {
             alert("Password nicht vorhanden");
             window.location.reload();
         }
@@ -53,9 +53,9 @@ function isExistAccount() {
 
 function login() {
     const password = document.querySelector("#password").value;
-    if(password == localStorage.getItem("password")) {
+    if (password == localStorage.getItem("password")) {
         const angemeldet = "true";
-        sessionStorage.setItem("angemeldet" , angemeldet)
+        sessionStorage.setItem("angemeldet", angemeldet)
         window.location = "/../system/v1/desktop/";
         loginGUI()
 
@@ -76,7 +76,7 @@ function loginGUI() {
     document.querySelector(".loader").style.display = "block";
     document.querySelector("#anmeldennachricht").style.display = "block";
     document.querySelector("#anmeldennachricht").innerHTML = "Willkommen " + localStorage.getItem("username");
-    window.setTimeout("weiterleitung()" , 100);
+    window.setTimeout("weiterleitung()", 100);
 }
 
 
@@ -98,26 +98,26 @@ function newPassword() {
     const validPassword = document.querySelector("#isValid").value;
     const error = document.querySelector("#error");
 
-    if(username == "") {
+    if (username == "") {
         error.innerHTML = "Gebe einen Benutzernamen ein";
     }
 
-    if(newPassword == "") {
-        error.innerHTML = "Gebe ein Passwort ein"; 
+    if (newPassword == "") {
+        error.innerHTML = "Gebe ein Passwort ein";
     }
 
-    if(newPassword !== validPassword) {
+    if (newPassword !== validPassword) {
         error.innerHTML = "Deine Passwörter stimmen nicht überein"
     }
 
-    if(username !== localStorage.getItem("username")) {
+    if (username !== localStorage.getItem("username")) {
         error.innerHTML = "Dieser User existiert nicht"
     } else {
-        if(newPassword == validPassword) {
-            localStorage.setItem("password" , newPassword)
+        if (newPassword == validPassword) {
+            localStorage.setItem("password", newPassword)
             window.location = "/../system/v1/lockscreen";
         }
-    }   
+    }
 }
 
 function register() {
@@ -127,26 +127,26 @@ function register() {
     const hintergrundibild = "noImage";
     const user = "registriert";
 
-    if(name == "") {
+    if (name == "") {
         const errormessage = document.querySelector("#errormessage_register");
         errormessage.innerHTML = "Bitte wähle einen Nutzernamen";
         return;
     }
-    
-    if(password == "") {
+
+    if (password == "") {
         const errormessage = document.querySelector("#errormessage_register");
         errormessage.innerHTML = "Bitte wähle ein Gültiges Passwort";
         return;
     }
 
-    if(profilbild == "") {
+    if (profilbild == "") {
         profilbild = "/../system/v1/standardbilder/profilbild.jpg"
     }
 
-    localStorage.setItem("registriert" , user)
-    localStorage.setItem("username" , name);
-    localStorage.setItem("password" , password);
-    localStorage.setItem("profilbild" , profilbild);
+    localStorage.setItem("registriert", user)
+    localStorage.setItem("username", name);
+    localStorage.setItem("password", password);
+    localStorage.setItem("profilbild", profilbild);
     localStorage.setItem("hintergrundbild", hintergrundibild);
     window.location = "/../system/v1/lockscreen"
 }
